de3e22db1a1a802f77628cad236aef59a71a9b88
Aram2142 <andresaaramirez@aol.com>
2021-04-10 00:11:27 -0400

    fixed writing for heading

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -7,31 +7,33 @@ import "./header.css";
 const Header = () => {
   return (
 
-    <div class="container">
-      <br></br>
-      <nav class="navbar navbar-expand-lg navbar-light bg-white">
-        <a className="navbar-brand" href="#">Shortly</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarText">
-          <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-              <a className="nav-link" href="#">Feautures</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">Pricing</a>
-            </li>
-            <li class="nav-item">
-              <a className="nav-link" href="#">Resources</a>
-            </li>
-          </ul>
-          <span class="nav-item ">
-            <a className="nav-link" href="#">Login</a>
-          </span>
-          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Sign Up</button>
-        </div>
-      </nav>
+    <div className={'header'}>
+      <div className={'container'}>
+
+        <nav class="navbar navbar-expand-lg navbar-light bg-white">
+          <a className="navbar-brand shortly" href="#">Shortly</a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarText">
+            <ul class="navbar-nav mr-auto choices">
+              <li class="nav-item">
+                <a className="nav-link" href="#">Features</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Pricing</a>
+              </li>
+              <li class="nav-item">
+                <a className="nav-link" href="#">Resources</a>
+              </li>
+            </ul>
+            <span class="nav-item ">
+              <a className="nav-link login" href="#">Login</a>
+            </span>
+            <button class="header-btn sign-up" type="submit">Sign Up</button>
+          </div>
+        </nav>
+      </div>
     </div>
   )
 }
