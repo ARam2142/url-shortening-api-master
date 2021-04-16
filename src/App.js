8eeb901e68d6f8fb87af60a5f3d81b57f7e37609
Aram2142 <andresaaramirez@aol.com>
2021-04-15 22:23:15 -0400

    i had to stop because i dont really know the basics in react

--- src/App.js
+++ src/App.js
@@ -5,13 +5,28 @@ import BoostLinks from "./components/boostlinks/BoostLinks"
 import Statistics from "./components/statistics/Statistics"
 import Footer from './components/footer/Footer';
 import './App.css';
+import React, { useState } from "react";
+import validator from 'validator'
 
 function App() {
+  const [errorMessage, setErrorMessage] = useState('')
+
+  const validate = (value) => {
+    if (validator.isURL(value)) {
+      setErrorMessage("Is valid rrl")
+    } else {
+      setErrorMessage("Is not a valid url")
+    }
+
+  }
+
+
+
   return (
     <div>
       <Header />
       <Hero />
-      <Input />
+      <Input validate={validate}/>
       <Statistics />
       <BoostLinks />
       <Footer />
--- src/components/input/Input.js
+++ src/components/input/Input.js
@@ -1,45 +1,52 @@
+import React, { useState, useEffect } from "react";
+// import axios from 'axios'
+import validator from 'validator'
+import "./inputApi.css"
+
+const Form = () => {
+    // const url = `https://api.shrtco.de/v2/shorten?url=${url}`
+    const [input, setInput] = useState('');
+
+    const handleChange = (e) => {
+        setInput(e.target.value)
+    }
+
+    const handleSubmit = (value, e) => {
+        e.preventDefault();
+        console.log(`The user typed in: ${input}`);
+        
+        
+        if (validator.isURL(value)) {
+            var expression = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;
+            var regexp = new RegExp(expression);
+            console.log(regexp)
+            return regexp.test(value)
+        }
+    }
+
+    return (
+        <div className="formbackground">
+            <form onSubmit={(e) => handleSubmit(e.target.value)}>
+                <input
+                    placeholder="Shorten a link here..."
+                    type="text"
+                    value={input}
+                    onChange={handleChange}
+                />
+                <button className="btn btn-primary shorten-it" type="submit"><span>Shorten It!</span></button>
+                {/* <p style={{
+                    fontWeight: "bold",
+                    color: "red"
+                }}>{ }</p> */}
+            </form>
+        </div>
+    )
+}
+
+export default Form;
+
 // Shorten any valid URL
 // See a list of their shortened links, even after refreshing the browser
 // Copy the shortened link to their clipboard in a single click
 // Receive an error message when the form is submitted if:
 // The input field is empty
-
-import React, {useState, useEffect} from "react";
-// import axios from 'axios'
-import "./inputApi.css"
-
-const CopyApi = () => {
-    const link = 'https://api.shrtco.de/v2/';
-    const [url, setUrl] = useState(['']);
-
-    useEffect(() => {
-        fetch(link)
-            .then(res => console.log(res))
-    })
-
-    function handleSubmit(e) {
-        e.preventDefault();
-        console.log(url);
-    }
-
-
-
-    return (
-        <div className="formbackground">
-            <form onSubmit={handleSubmit}>
-                <input placeholder="Shorten a link here..." value={url} type="text" onChange={(e) =>setUrl(e.target.value) }/>
-                <button className="btn btn-primary shorten-it" type="submit"><span>Shorten It!</span></button>
-            </form>
-            {/* <ul>
-                {urls.map(url => ( 
-                    <li key={url.id}>
-                        {}
-                    </li>
-                ))}
-            </ul> */}
-
-        </div>
-    )
-}
-
-export default CopyApi;
